ec7a8ce0c175b73bd8fc84284e8ff771f896de49
Jwest75 <jonathanwest303@gmail.com>
2022-10-31 20:44:22 -0700

    take home

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// single line comment
+let name = "jonathan"
+
+let age = "47"
+/*mutli line comment
+go 
+bo
+*/
+const intersestRate = 2.5
+
+let score;
+score=50
+console.log( "score is:" ,score);
+score=65
+console.log("score is:",score);
+let food = "Bar B Q"
+console.log(food)
+
+
+console.log(name);
+console.log(age);
+console.log(intersestRate);
